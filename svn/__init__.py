450dfa96d7e68e80412d5b66b8a6de6a2bbd819c
Dustin Oprea <myselfasunder@gmail.com>
2014-03-31 23:19:09 -0400

    Removed remaining bit of Versioneer.

--- svn/__init__.py
+++ svn/__init__.py
@@ -3,6 +3,6 @@ from svn.remote import RemoteClient
 
 (T_URL, T_PATH) = ('url', 'path')
 
-from ._version import get_versions
-__version__ = get_versions()['default']
-del get_versions
+#from ._version import get_versions
+#__version__ = get_versions()['default']
+#del get_versions
