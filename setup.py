66174eb6821452b5ffd0c253f264fcf9a6d9940c
Dustin Oprea <myselfasunder@gmail.com>
2014-07-07 15:25:33 -0400

    Convert the generator to a list for requirements.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(_APP_PATH, 'resources', 'README.rst')) as f:
       long_description = ''.join(f.readlines())
 
 with open(os.path.join(_APP_PATH, 'resources', 'requirements.txt')) as f:
-      install_requires = map(lambda s: s.strip(), f.readlines())
+      install_requires = list(map(lambda s: s.strip(), f.readlines()))
 
 setuptools.setup(
     name='svn',
