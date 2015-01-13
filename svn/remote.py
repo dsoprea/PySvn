b8e03b573ec5f486272cd86d1665fd88a33ba00a
Juan Pedro Escalona <jescalona@riplife.es>
2015-01-13 09:34:23 +0100

    Added ability to set credentials in order to authenticate to the repo

--- svn/remote.py
+++ svn/remote.py
@@ -4,7 +4,9 @@ from svn import common
 
 
 class RemoteClient(common.CommonClient):
-    def __init__(self, url, *args, **kwargs):
+    def __init__(self, url, username=None, password=None, *args, **kwargs):
+        self.username = username
+        self.password = password
         super(RemoteClient, self).__init__(url, svn.T_URL, *args, **kwargs)
 
     def checkout(self, path, revision=None):
@@ -13,6 +15,9 @@ class RemoteClient(common.CommonClient):
             cmd += ['-r', str(revision)]
 
         cmd += [self.url, path]
+        if self.username is not None:
+            cmd += ['--username', self.username]
+            cmd += ['--password', self.password]
 
         self.run_command('checkout', cmd)
 
