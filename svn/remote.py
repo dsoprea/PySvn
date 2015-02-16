eb2c60a5b18f566cc43b876bb5cf4cc2fc6bf73e
Juan Pedro Escalona <jescalona@riplife.es>
2015-02-16 11:05:37 +0100

    Moved credentials to CommonClient to make it more clean

--- svn/remote.py
+++ svn/remote.py
@@ -6,18 +6,12 @@ from svn import common
 class RemoteClient(common.CommonClient):
 
     def __init__(self, url, *args, **kwargs):
-        self.username = kwargs.pop('username', None)
-        self.password = kwargs.pop('password', None)
         super(RemoteClient, self).__init__(url, svn.T_URL, *args, **kwargs)
 
     def checkout(self, path, revision=None):
         cmd = []
         if revision is not None:
             cmd += ['-r', str(revision)]
-
-        if self.username is not None:
-            cmd += ['--username', self.username]
-            cmd += ['--password', self.password]
 
         cmd += [self.url, path]
 
