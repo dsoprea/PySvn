db572812b85ad00c2014f72b0b8d1a32de1cec59
Dustin Oprea <myselfasunder@gmail.com>
2016-04-14 17:49:49 -0400

    We now forward arguments from get_client().

--- svn/utility.py
+++ svn/utility.py
@@ -1,8 +1,8 @@
 import svn.local
 import svn.remote
 
-def get_client(url_or_path):
+def get_client(url_or_path, *args, **kwargs):
     if url_or_path[0] == '/':
-        return svn.local.LocalClient(url_or_path)
+        return svn.local.LocalClient(url_or_path, *args, **kwargs)
     else:
-        return svn.remote.RemoteClient(url_or_path)
+        return svn.remote.RemoteClient(url_or_path, *args, **kwargs)
