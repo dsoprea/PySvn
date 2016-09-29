baec218312f17b71bb8baf82e8ecc85b8ee92837
Tushar Makkar <tushar@codenation.co.in>
2016-09-29 20:05:27 +0530

    Fixing @dsoprea's comment

--- svn/common.py
+++ svn/common.py
@@ -1,5 +1,4 @@
 import collections
-import copy
 import logging
 import os
 import subprocess
@@ -51,7 +50,7 @@ class CommonClient(object):
 
         _logger.debug("RUN: %s" % (cmd,))
 
-        environment_variables = copy.deepcopy(os.environ)
+        environment_variables = os.environ.copy()
         environment_variables['LANG'] = 'en_US.UTF-8'
 
         p = subprocess.Popen(cmd,
