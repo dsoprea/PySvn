27d80113f005b356d0a10ff3b8ed0c0ee9ed9048
Dustin Oprea <myselfasunder@gmail.com>
2020-02-01 15:52:57 -0500

    Converted documentation to Markdown

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import svn
 
 _APP_PATH = os.path.dirname(svn.__file__)
 
-with open(os.path.join(_APP_PATH, 'resources', 'README.rst')) as f:
+with open(os.path.join(_APP_PATH, 'resources', 'README.md')) as f:
     long_description = f.read()
 
 with open(os.path.join(_APP_PATH, 'resources', 'requirements.txt')) as f:
@@ -16,6 +16,7 @@ setuptools.setup(
     version=svn.__version__,
     description="Intuitive Subversion wrapper.",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[],
     keywords='svn subversion',
     author='Dustin Oprea',
@@ -26,8 +27,10 @@ setuptools.setup(
     include_package_data=True,
     zip_safe=False,
     package_data={
-        'svn': ['resources/README.rst',
-                'resources/requirements.txt'],
+        'svn': [
+            'resources/README.md',
+            'resources/requirements.txt',
+        ],
     },
     install_requires=install_requires,
 )
