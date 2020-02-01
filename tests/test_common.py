606bf2cd76ac4d5b70894125d8a87daf02933dc2
Dustin Oprea <myselfasunder@gmail.com>
2020-02-01 15:52:46 -0500

    test_common.py: Refactor to use context-manager

--- tests/test_common.py
+++ tests/test_common.py
@@ -3,6 +3,7 @@ import unittest
 import shutil
 import tempfile
 import logging
+import contextlib
 
 from tests.resources.expected_output import diff_summary, diff_summary_2, cat
 
@@ -12,6 +13,8 @@ import svn.common
 import svn.local
 import svn.remote
 import svn.admin
+
+_TEST_APACHE_SVN_URL = 'http://svn.apache.org/repos/asf'
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,12 +37,39 @@ class TestCommonClient(unittest.TestCase):
 
         return temp_path
 
-    def setUp(self):
-        self.test_svn_url = 'http://svn.apache.org/repos/asf'
-        self.test_start_revision = 1760022
-        self.test_end_revision = 1760023
+    def __cleanup_test_environment(self):
+        try:
+            shutil.rmtree(self.__temp_co_path)
+        except:
+            _LOGGER.exception("Could not cleanup temporary checkout path: [%s]", self.__temp_co_path)
 
+        try:
+            shutil.rmtree(self.__temp_repo_path)
+        except:
+            _LOGGER.exception("Could not cleanup temporary repository path: [%s]", self.__temp_repo_path)
+
+    @contextlib.contextmanager
+    def _stage_repo(self):
         self.__setup_test_environment()
+        cc = svn.common.CommonClient('file://' + self.__temp_repo_path, 'url')
+
+        try:
+            yield cc
+        finally:
+            self.__cleanup_test_environment()
+
+    @contextlib.contextmanager
+    def _local_populated_test(self):
+        self.__setup_test_environment()
+        cc = svn.common.CommonClient('file://' + self.__temp_repo_path, 'url')
+
+        try:
+            self.__stage_co_directory_1()
+            self.__stage_big_diff()
+
+            yield cc
+        finally:
+            self.__cleanup_test_environment()
 
     def __setup_test_environment(self):
 
@@ -60,21 +90,6 @@ class TestCommonClient(unittest.TestCase):
 
         # Create a client for it.
         self.__temp_lc = svn.local.LocalClient(self.__temp_co_path)
-
-    def tearDown(self):
-# TODO(dustin): !! This has to be refactored to use a random temporary file.
-        if os.path.exists('CHANGES'):
-            shutil.rmtree('CHANGES')
-
-        try:
-            shutil.rmtree(self.__temp_co_path)
-        except:
-            _LOGGER.exception("Could not cleanup temporary checkout path: [%s]", self.__temp_co_path)
-
-        try:
-            shutil.rmtree(self.__temp_repo_path)
-        except:
-            _LOGGER.exception("Could not cleanup temporary repository path: [%s]", self.__temp_repo_path)
 
     def __stage_co_directory_1(self):
         """Establish a new file, an added file, a committed file, and a changed file."""
@@ -116,11 +131,9 @@ class TestCommonClient(unittest.TestCase):
         self.__temp_lc.add(rel_filepath_deleted)
 
         # Commit the new files.
-
         self.__temp_lc.commit("Initial commit.")
 
         # Do an update to pick-up the changes from the commit.
-
         self.__temp_lc.update()
 
         # Change the one committed file so that it will show up as modified.
@@ -129,7 +142,6 @@ class TestCommonClient(unittest.TestCase):
             f.write("new data")
 
         # Delete the one committed file so that it will show up as deleted.
-
         os.unlink(filepath_deleted)
 
         # Create a file that will be added and not committed.
@@ -141,32 +153,135 @@ class TestCommonClient(unittest.TestCase):
 
         self.__temp_lc.add(rel_filepath)
 
+    def __stage_big_diff(self):
+        """Upload a file having many lines and then do another commit with
+        several changed.
+        """
+
+        # Create a file that will be committed and then changed a lot.
+
+        rel_filepath_changed_big = 'committed_changed_big'
+        filepath_changed_big = os.path.join(self.__temp_co_path, rel_filepath_changed_big)
+        with open(filepath_changed_big, 'w') as f:
+            f.write("""\
+Lorem ipsum
+dolor sit
+amet, consectetur
+adipiscing elit,
+sed do
+eiusmod tempor
+incididunt ut
+labore et
+dolore magna
+aliqua. Ut
+enim ad
+minim veniam,
+quis nostrud
+exercitation ullamco
+laboris nisi
+ut aliquip
+ex ea
+commodo consequat.
+Duis aute
+irure dolor
+in reprehenderit
+in voluptate
+velit esse
+cillum dolore
+eu fugiat
+nulla pariatur.
+Excepteur sint
+occaecat cupidatat
+non proident,
+sunt in
+culpa qui
+officia deserunt
+mollit anim
+id est
+laborum."
+""")
+
+        self.__temp_lc.add(rel_filepath_changed_big)
+
+        # Commit the new files.
+        self.__temp_lc.commit("Initial commit.")
+
+        # Apply the bigger change.
+
+        with open(filepath_changed_big, 'w') as f:
+            f.write("""\
+Lorem ipsum
+dolor sit
+amet, consectetur
+adipiscing elit,
+sed do
+!newline1
+eiusmod tempor
+incididunt ut
+labore et
+dolore magna
+aliqua. Ut
+enim ad
+minim veniam,
+quis nostrud
+exercitation ullamco
+laboris nisi
+ut aliquip
+ex ea
+commodo consequat.
+Duis aute
+irure dolor
+in reprehenderit
+in voluptate
+velit esse
+cillum dolore
+eu fugiat
+nulla pariatur.
+Excepteur sint
+occaecat cupidatat
+!newline2
+non proident,
+sunt in
+culpa qui
+officia deserunt
+mollit anim
+id est
+laborum."
+""")
+
+        self.__temp_lc.commit("A bigger change.")
+
+        # Do an update to pick-up the changes from the commit.
+        self.__temp_lc.update()
+
     def test_status(self):
-        self.__stage_co_directory_1()
+        with self._stage_repo():
+            self.__stage_co_directory_1()
 
-        status = {}
-        for s in self.__temp_lc.status():
-            _LOGGER.debug("STATUS: %s", s)
+            status = {}
+            for s in self.__temp_lc.status():
+                _LOGGER.debug("STATUS: %s", s)
 
-            filename = os.path.basename(s.name)
-            status[filename] = s
+                filename = os.path.basename(s.name)
+                status[filename] = s
 
-        added = status['added']
-        self.assertTrue(added is not None and added.type == svn.constants.ST_ADDED)
+            added = status['added']
+            self.assertTrue(added is not None and added.type == svn.constants.ST_ADDED)
 
-        committed_changed = status['committed_changed']
-        self.assertTrue(committed_changed is not None and committed_changed.type == svn.constants.ST_MODIFIED)
+            committed_changed = status['committed_changed']
+            self.assertTrue(committed_changed is not None and committed_changed.type == svn.constants.ST_MODIFIED)
 
-        committed_deleted = status['committed_deleted']
-        self.assertTrue(committed_deleted is not None and committed_deleted.type == svn.constants.ST_MISSING)
+            committed_deleted = status['committed_deleted']
+            self.assertTrue(committed_deleted is not None and committed_deleted.type == svn.constants.ST_MISSING)
 
     def test_update(self):
-        self.__stage_co_directory_1()
-        self.__temp_lc.commit("Second commit.")
-        self.__temp_lc.update()
-        self.assertEqual(2, self.__temp_lc.info()['commit_revision'])
-        self.__temp_lc.update(revision=1)
-        self.assertEqual(1, self.__temp_lc.info()['commit_revision'])
+        with self._stage_repo():
+            self.__stage_co_directory_1()
+            self.__temp_lc.commit("Second commit.")
+            self.__temp_lc.update()
+            self.assertEqual(2, self.__temp_lc.info()['commit_revision'])
+            self.__temp_lc.update(revision=1)
+            self.assertEqual(1, self.__temp_lc.info()['commit_revision'])
 
     def test_error_client_formation(self):
         """
@@ -174,143 +289,169 @@ class TestCommonClient(unittest.TestCase):
         :return:
         """
         with self.assertRaises(svn.exception.SvnException):
-            svn.common.CommonClient(self.test_svn_url, 'random')
+            svn.common.CommonClient(_TEST_APACHE_SVN_URL, 'random')
 
-    def __get_cc(self):
-        return svn.common.CommonClient(self.test_svn_url, 'url')
+    def __get_apache_cc(self):
+        return svn.common.CommonClient(_TEST_APACHE_SVN_URL, 'url')
 
     def test_diff_summary(self):
-        """
-        Checking diff summary
-        :return:
-        """
-        cc = self.__get_cc()
+        self._test_start_revision = 1760022
+        self._test_end_revision = 1760023
+
+        cc = self.__get_apache_cc()
         actual_answer = \
             cc.diff_summary(
-                self.test_start_revision,
-                self.test_end_revision)
+                self._test_start_revision,
+                self._test_end_revision)
 
-        self.assertTrue(
-            actual_answer == diff_summary or \
-            actual_answer == diff_summary_2)
+        with self._stage_repo():
+            cc = self.__get_apache_cc()
+            actual_answer = \
+                cc.diff_summary(
+                    self._test_start_revision,
+                    self._test_end_revision)
+
+            self.assertTrue(
+                actual_answer == diff_summary or \
+                actual_answer == diff_summary_2)
 
     def test_diff(self):
         """
         Checking diff
         :return:
         """
-        cc = self.__get_cc()
-        actual_answer = \
-            cc.diff(self.test_start_revision, self.test_end_revision)
 
-        for index, individual_diff in enumerate(actual_answer):
-            for diff_key in individual_diff:
-                if diff_key == 'diff':
-                    self.assertTrue(
-                        'sling/trunk/bundles/extensions/models/pom.xml' \
-                            in individual_diff[diff_key] or \
-                        'sling/trunk/pom.xml' \
-                            in individual_diff[diff_key])
-                    self.assertTrue(
-                        '<module>bundles/extensions/models</module>' \
-                            in individual_diff[diff_key] or
-                        '<description>Apache Sling Models</description>' \
-                            in individual_diff[diff_key])
-                elif diff_key == 'path':
-                    self.assertTrue(
-                        'http://svn.apache.org/repos/asf/sling/trunk/bundles/extensions/models/pom.xml' \
-                            in individual_diff[diff_key] or \
-                        'http://svn.apache.org/repos/asf/sling/trunk/pom.xml' \
-                            in individual_diff[diff_key])
+        with self._apache_test():
+            cc = self.__get_cc()
+            actual_answer = \
+                cc.diff(self.test_start_revision, self.test_end_revision)
+
+            for index, individual_diff in enumerate(actual_answer):
+                for diff_key in individual_diff:
+                    if diff_key == 'diff':
+                        self.assertTrue(
+                            'sling/trunk/bundles/extensions/models/pom.xml' \
+                                in individual_diff[diff_key] or \
+                            'sling/trunk/pom.xml' \
+                                in individual_diff[diff_key])
+                        self.assertTrue(
+                            '<module>bundles/extensions/models</module>' \
+                                in individual_diff[diff_key] or
+                            '<description>Apache Sling Models</description>' \
+                                in individual_diff[diff_key])
+                    elif diff_key == 'path':
+                        self.assertTrue(
+                            'http://svn.apache.org/repos/asf/sling/trunk/bundles/extensions/models/pom.xml' \
+                                in individual_diff[diff_key] or \
+                            'http://svn.apache.org/repos/asf/sling/trunk/pom.xml' \
+                                in individual_diff[diff_key])
 
     def test_list(self):
         """
         Checking list
         :return:
         """
-        cc = self.__get_cc()
-        actual_answer = cc.list()
-        self.assertEqual(next(actual_answer), 'abdera/')
+
+        with self._stage_repo():
+            cc = self.__get_apache_cc()
+            actual_answer = cc.list()
+            self.assertEqual(next(actual_answer), 'abdera/')
 
     def test_info(self):
         """
         Checking info
         :return:
         """
-        cc = self.__get_cc()
-        actual_answer = cc.info()
 
-        self.assertEqual(
-            actual_answer['entry_path'],
-            'asf')
+        with self._stage_repo():
+            cc = self.__get_apache_cc()
+            actual_answer = cc.info()
 
-        self.assertEqual(
-            actual_answer['repository_root'],
-            'http://svn.apache.org/repos/asf')
+            self.assertEqual(
+                actual_answer['entry_path'],
+                'asf')
 
-        self.assertEqual(
-            actual_answer['entry#kind'],
-            'dir')
+            self.assertEqual(
+                actual_answer['repository_root'],
+                'http://svn.apache.org/repos/asf')
 
-        self.assertEqual(
-            actual_answer['repository/uuid'],
-            '13f79535-47bb-0310-9956-ffa450edef68')
+            self.assertEqual(
+                actual_answer['entry#kind'],
+                'dir')
+
+            self.assertEqual(
+                actual_answer['repository/uuid'],
+                '13f79535-47bb-0310-9956-ffa450edef68')
 
     def test_info_revision(self):
-        cc = self.__get_cc()
-        actual_answer = cc.info(revision=1000000)
-        self.assertEqual(actual_answer['commit_revision'], 1000000)
-        self.assertEqual(actual_answer['commit_author'], 'yonik')
+        with self._stage_repo():
+            cc = self.__get_apache_cc()
+            actual_answer = cc.info(revision=1000000)
+            self.assertEqual(actual_answer['commit_revision'], 1000000)
+            self.assertEqual(actual_answer['commit_author'], 'yonik')
 
     def test_log(self):
         """
         Checking log
         :return:
         """
-        cc = self.__get_cc()
-        actual_answer = \
-            cc.log_default(revision_from=1761404, revision_to=1761403)
-        self.assertEqual(next(actual_answer).author, 'sseifert')
+
+        with self._stage_repo():
+            cc = self.__get_apache_cc()
+            actual_answer = \
+                cc.log_default(revision_from=1761404, revision_to=1761403)
+            self.assertEqual(next(actual_answer).author, 'sseifert')
 
     def test_cat(self):
         """
         Checking cat
         :return:
         """
-        cc = self.__get_cc()
-        actual_answer = cc.cat('abdera/abdera2/README', revision=1761404)
-        self.assertEqual(cat, actual_answer.decode())
+
+        with self._stage_repo():
+            cc = self.__get_apache_cc()
+            actual_answer = cc.cat('abdera/abdera2/README', revision=1761404)
+            self.assertEqual(cat, actual_answer.decode())
 
     def test_export(self):
         """
         Checking export
         :return:
         """
-        cc = \
-            svn.common.CommonClient(
-                'http://svn.apache.org/repos/asf/tcl/websh/trunk/',
-                'url')
 
-        cc.export(to_path='CHANGES', revision=1761404)
-        self.assertTrue(os.path.exists('CHANGES'))
+        with self._stage_repo():
+            cc = \
+                svn.common.CommonClient(
+                    'http://svn.apache.org/repos/asf/tcl/websh/trunk/',
+                    'url')
+
+            try:
+                cc.export(to_path='CHANGES', revision=1761404)
+                self.assertTrue(os.path.exists('CHANGES'))
+            finally:
+                if os.path.exists('CHANGES'):
+                    shutil.rmtree('CHANGES')
 
     def test_force_export(self):
         """
         Checking export with force option
         :return:
         """
-        cc = svn.common.CommonClient('http://svn.apache.org/repos/asf/tcl/websh/trunk/', 'url')
-        cc.export(to_path='CHANGES', revision=1761404)
-        self.assertTrue(os.path.exists('CHANGES'))
 
-        with self.assertRaises(svn.exception.SvnException):
-            cc.export(to_path='CHANGES', revision=1761404)
-        try:
-            cc.export(to_path='CHANGES', revision=1761404, force=True)
-# TODO(dustin): This except probably unnecessary (any exception should likely
-#               trigger failure).
-        except svn.exception.SvnException:
-            self.fail("SvnException raised with force export")
+        with self._stage_repo():
+            try:
+                cc = svn.common.CommonClient('http://svn.apache.org/repos/asf/tcl/websh/trunk/', 'url')
+                cc.export(to_path='CHANGES', revision=1761404)
+                self.assertTrue(os.path.exists('CHANGES'))
+
+                with self.assertRaises(svn.exception.SvnException):
+                    cc.export(to_path='CHANGES', revision=1761404)
+
+                cc.export(to_path='CHANGES', revision=1761404, force=True)
+            finally:
+                if os.path.exists('CHANGES'):
+                    shutil.rmtree('CHANGES')
 
     def test_cleanup(self):
-        self.__temp_lc.cleanup()
+        with self._stage_repo():
+            self.__temp_lc.cleanup()
